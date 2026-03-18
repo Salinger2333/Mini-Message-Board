4575f6084483d934af0837cb4f6afaa52eebb62c
Salinger2333 <1565664814@qq.com>
2026-03-18 21:35:02 +0800

    add neon config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import newRouter from "./routes/newRoutes.js";
 import indexRouter from "./routes/indexRoutes.js";
-import path from 'node:path';
+import path from "node:path";
+import "dotenv/config";
+import { neon } from "@neondatabase/serverless";
+
+
+const sql = neon(process.env.DATABASE_URL);
+const result = await sql`SELECT version()`;
+console.log(result[0]);
 const app = express();
 
 const viewsPath = path.join(import.meta.dirname, "views");
@@ -14,7 +21,7 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
-app.use('/new',newRouter)
+app.use("/new", newRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
